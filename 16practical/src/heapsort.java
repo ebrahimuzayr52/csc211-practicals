cfbe0fbfe9954520aa4875f7b2acd518b818d9c2
ebrahimuzayr52 <uzayre567@gmail.com>
2026-03-16 14:27:34 +0200

    hhh

---
+++ 16practical/src/heapsort.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+
+public class heapsort {
+    static void shift_Down(String[] arr, int i, int size) {
+        while(true){
+            int left = 2*i+2;
+            int right= 2*i+1;
+            int parent= i+1;
+        }
+
+
+
+
+
+    }
+}
